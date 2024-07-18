167a2b932c0c0df2d07ee748f878ff3360e2c6ea
mnasiri <mnasiri_gc@yahoo.com>
2024-07-18 10:46:05 +0330

    demo of component with hard coded data

--- src/components/province-select/ProvinceMapApp.tsx
+++ src/components/province-select/ProvinceMapApp.tsx
@@ -2,13 +2,12 @@ import {IranProvincesMap} from "react-iran-provinces-map";
 import './layoutmap.css';
 import {ProvinceMapData} from "react-iran-provinces-map/dist/esm/interfaces"
 import {selectedCityType} from "react-iran-provinces-map/dist/cjs/interfaces";
-import {mapDataType, selectedProvinceType} from "react-iran-map/dist/esm/interfaces";
+import {selectedProvinceType} from "react-iran-map/dist/esm/interfaces";
 
 
 interface PMAppProps {
-    // thePMapData: ProvinceMapData
-    curMapData: mapDataType
-    selectedProvince: selectedProvinceType | undefined
+    thePMapData: ProvinceMapData
+    selectedProvince: selectedProvinceType | undefined;
 }
 
 
@@ -16,16 +15,15 @@ function PApp(defaultData: PMAppProps) {
     console.log(defaultData)
     let selectedProvince: selectedProvinceType | undefined = defaultData.selectedProvince;
     let selectedCity: string | undefined = undefined
-    let curData: any = defaultData.curMapData
-    // if (selectedProvince?.name)
-    //     curData = defaultData.thePMapData[selectedProvince.name]
+    let curData: any = undefined
+    if (selectedProvince?.name)
+        curData = defaultData.thePMapData[selectedProvince.name]
 
     const selectProvinceHandler = (city: selectedCityType) => {
         selectedCity = city.name
         console.log(selectedCity, city.faName)
 
     };
-    {/*"30, 70, 181"*/}
 
     if (selectedProvince?.name)
         return (
@@ -37,7 +35,7 @@ function PApp(defaultData: PMAppProps) {
                     colorRange="59, 204, 109"
                     deactiveProvinceColor="#eee"
                     selectedProvinceColor="#fc6b9d"
-                    tooltipTitle="تعداد بقاع:"
+                    tooltipTitle="تعداد:"
                     textColor="#333"
                     width={600}
                     selectProvinceHandler={selectProvinceHandler}
--- src/components/province-select/layout-map.tsx
+++ src/components/province-select/layout-map.tsx
@@ -2,42 +2,19 @@ import './layoutmap.css';
 import MApp from './MapApp';
 import PApp from './ProvinceMapApp';
 import React, {useEffect} from "react";
+import {mapData} from '../../data/list'
+import {defaultData} from '../../data/defaultData'
 import {useState} from "react";
 
 function App() {
     // const default_province = {name: "tehran", faName: "تهران"}
-    const [selectedProvince, setSelectedProvince] = useState({name: undefined, faName:undefined});
-    const [mapData, setMapData] = useState({});
-    const [pMapData, setPMapData] = useState({});
-
-    useEffect(() => {
-        fetch('http://127.0.0.1:5000/provinces')
-            .then(response => response.json())
-            .then(json => {
-                setMapData(json);
-                console.log(json);
-            })
-            .catch(error => console.error(error));
-    }, []);
-
-    useEffect(() => {
-        let p_name = selectedProvince?.name
-        if (p_name !== undefined)
-            fetch('http://127.0.0.1:5000/provinces/' + selectedProvince.name)
-                .then(response => response.json())
-                .then(json => {
-                    setPMapData(json);
-                    console.log(json);
-                })
-                .catch(error => console.error(error));
-    }, [selectedProvince]);
+    const [selectedProvince, setSelectedProvince] = useState();
 
     return (
         <div>
             <div className="root">
-                <PApp curMapData={pMapData} selectedProvince={selectedProvince}/>
-                <MApp theMapData={mapData} selectedProvince={selectedProvince}
-                      setSelectedProvince={setSelectedProvince}/>
+                <PApp thePMapData={defaultData} selectedProvince={selectedProvince}/>
+                <MApp theMapData={mapData} selectedProvince={selectedProvince} setSelectedProvince={setSelectedProvince}/>
             </div>
         </div>
     )
