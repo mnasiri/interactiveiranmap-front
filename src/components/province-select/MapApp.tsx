4a64e733dbfac63a12645119b3a5da1888fc2cbf
mnasiri <mnasiri_gc@yahoo.com>
2024-07-18 10:40:50 +0330

    the code's first version

---
+++ src/components/province-select/MapApp.tsx
@@ -0,0 +1,46 @@
+import {IranMap} from 'react-iran-map'
+import './layoutmap.css';
+import {mapDataType, selectedProvinceType} from "react-iran-map/dist/esm/interfaces";
+
+
+interface MAppProps {
+    theMapData: mapDataType;
+    selectedProvince: selectedProvinceType | undefined;
+    setSelectedProvince: Function | undefined;
+
+}
+
+function MApp(mdata: MAppProps) {
+    console.log(mdata)
+    let selectedProvince = mdata.selectedProvince
+
+    const selectProvinceHandler = (province: selectedProvinceType) => {
+        console.log(province)
+        if (province.name) {
+            selectedProvince = province
+            if (mdata.setSelectedProvince)
+                mdata.setSelectedProvince(selectedProvince)
+
+        }
+        console.log(selectedProvince, province.faName)
+
+    }
+
+    return (
+        <div style={{fontFamily: 'vazir'}} className="map-item">
+            <IranMap
+                data={mdata.theMapData}
+                colorRange='30, 70, 181'
+                width={600}
+                textColor='#000'
+                defaultSelectedProvince=''
+                deactiveProvinceColor='#eee'
+                selectedProvinceColor='#3bcc6d'
+                tooltipTitle='تعداد:'
+                selectProvinceHandler={selectProvinceHandler}
+            />
+        </div>
+    )
+}
+
+export default MApp
---
+++ src/components/province-select/ProvinceMapApp.tsx
@@ -0,0 +1,49 @@
+import {IranProvincesMap} from "react-iran-provinces-map";
+import './layoutmap.css';
+import {selectedCityType} from "react-iran-provinces-map/dist/cjs/interfaces";
+import {mapDataType, selectedProvinceType} from "react-iran-map/dist/esm/interfaces";
+
+
+interface PMAppProps {
+    curMapData: mapDataType
+    selectedProvince: selectedProvinceType | undefined
+}
+
+
+function PApp(defaultData: PMAppProps) {
+    console.log(defaultData)
+    let selectedProvince: selectedProvinceType | undefined = defaultData.selectedProvince;
+    let selectedCity: string | undefined = undefined
+    let curData: any = defaultData.curMapData
+    // if (selectedProvince?.name)
+    //     curData = defaultData.thePMapData[selectedProvince.name]
+
+    const selectProvinceHandler = (city: selectedCityType) => {
+        selectedCity = city.name
+        console.log(selectedCity, city.faName)
+
+    };
+    {/*"30, 70, 181"*/}
+
+    if (selectedProvince?.name)
+        return (
+            <div style={{fontFamily: "vazir"}} className="map-item">
+                <h1 className="province-header">{selectedProvince.faName}</h1>
+                <IranProvincesMap
+                    province={selectedProvince.name}
+                    provinceData={curData}
+                    colorRange="59, 204, 109"
+                    deactiveProvinceColor="#eee"
+                    selectedProvinceColor="#fc6b9d"
+                    tooltipTitle="تعداد بقاع:"
+                    textColor="#333"
+                    width={600}
+                    selectProvinceHandler={selectProvinceHandler}
+                />
+            </div>
+        );
+    else
+        return (<div/>)
+}
+
+export default PApp;
